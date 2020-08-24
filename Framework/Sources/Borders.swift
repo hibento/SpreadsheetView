fff139b0d047a779dec6691a6100c4a5401840c0
Christian Steffens <cs@hibento.de>
2020-08-24 16:55:37 +0200

    Migrates to Swift-5

--- Framework/Sources/Borders.swift
+++ Framework/Sources/Borders.swift
@@ -29,8 +29,8 @@ extension BorderStyle: Equatable {
         switch (lhs, rhs) {
         case (.none, .none):
             return true
-        case let (.solid(lhs), .solid(rhs)):
-            return lhs.width == rhs.width && lhs.color == rhs.color
+        case let (.solid(lhsWidth, lhsColor), .solid(rhsWidth, rhsColor)):
+            return lhsWidth == rhsWidth && lhsColor == rhsColor
         default:
             return false
         }
